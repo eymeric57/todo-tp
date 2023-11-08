5e7f51b2aa9a24b4fc5f94123da5cb81d9b8d50f
unknown <eymeric57160@hotmail.fr>
2023-11-08 13:41:00 +0100

    work in progress

---
+++ index.js
@@ -0,0 +1,40 @@
+const monInput = document.querySelector("input");
+
+const form = document.getElementById("form");
+const todoList = document.getElementById("list");
+
+function storeList() {
+    window.localStorage.todoList = todoList.innerHTML;
+  }
+let todo = "";
+
+monInput.addEventListener("input", (e) => {
+  todo = e.target.value;
+ 
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (todo) {
+    const data = {
+      todo,
+    };
+    
+    todoList.innerHTML += `<li> ${todo} </li>`;
+    monInput.value = ''; 
+}
+storeList();
+});
+
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("checked")) {
+    e.target.remove();
+  } else {
+    e.target.classList.add("checked");
+  }
+  storeList();
+});
+
+
+
